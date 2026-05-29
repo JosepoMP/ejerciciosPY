4bfda7d191283d5c6778154ac961780cabfa21eb
Esteban <esteban.bedoya@keybe.ai>
2026-05-28 23:53:56 -0500

    Segundocommit

---
+++ ejerciciosPY/prueba.py
@@ -0,0 +1,18 @@
+edadRichardBranson = 75
+edadJeffBezos = 62
+edadElonMusk = 54
+
+Promedioedad = (edadJeffBezos+edadElonMusk+edadRichardBranson)/3
+print("La edad promedio es:",Promedioedad)
+
+
+
+precioProducto = 200
+print(precioProducto)
+print(type(precioProducto))
+
+
+precioProducto = 305.50
+print(precioProducto)
+print(type(precioProducto))
+
